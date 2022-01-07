ba93f694cf01e6938438b2c1d8c739e66d42ea16
ROBOTICSENGINEER <mohsen.jafarzade@gmail.com>
2022-01-07 10:25:04 -0700

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -1,3 +1,45 @@
+'''
+Requirment:
+
+python 3.8
+numpy
+pandas
+
+============
+
+Running:
+
+You can run by entering following command
+
+ python3.8 test_1.py --model 1 --length 100 --redlight 40 --noise_threshold 0.5 --output_csv_path output_1.csv
+
+
+============
+
+
+--model: is an integer between 1 to 10.
+
+model 1: linear, ignoring true red light
+model 2: Quadratic, ignoring true red light
+model 3: Square Root, ignoring true red light
+model 4: Sin, ignoring true red light
+model 5: Circle, ignoring true red light
+model 6: Cumulative Sum, ignoring true red light
+model 7: Perturbation, similar to 6 but jump to 1 aftter red light is on
+model 8: Smart, return true red light
+model 9: Gaussian, similar to 8 but follow Gassuian CDF, it is 0.5 on first red light
+model 10: Gaussian Bernoulli, has corrolatation with red light, no causation. may go 0.5 before or after fist red light.
+
+--length:lenght of test. integer number equal or greater than 10.
+
+--redlight: first time redlight is on. positive integer number. value 1 means first index is also on. you can subtract from 1 to get index in python array.
+
+--noise_threshold: float number between 0.0 and 1.0. default is 0.5. It is used in Cumulative Sum and Perturbation models. It does not affect other models.
+
+--output_csv_path: path for output csv file.
+
+'''
+
 import numpy as np
 import pandas as pd
 from scipy.stats import norm
@@ -53,12 +95,8 @@ elif args.model == 6:
   d = v / (N / 2)
   y = np.cumsum(d)
   y[y>1.0] = 1.0
-  
+
 elif args.model == 7:
-  print("model: Smart")
-  y = true_red_light
-  
-elif args.model == 8:
   print("model: Perturbation")
   x = np.random.uniform(low=0.0, high=1.0, size=N)
   v = (x>args.noise_threshold).astype(np.float64)
@@ -67,7 +105,16 @@ elif args.model == 8:
   y[y>1.0] = 1.0
   y[index_redlight:] = 1.0
   
+elif args.model == 8:
+  print("model: Smart")
+  y = true_red_light
+  
 elif args.model == 9:
+  print("model: Gaussian")
+  x = np.arange(N)
+  y = norm.cdf(x, loc=index_redlight, scale=N/4)
+  
+elif args.model == 10:
   print("model: Gaussian Bernoulli")
   G = norm.cdf(np.arange(N), loc=index_redlight, scale=N/4)
   x = np.zeros(N)
@@ -79,10 +126,6 @@ elif args.model == 9:
   y = z / (2 * z[index_redlight])
   y[y>1.0] = 1.0
   
-elif args.model == 10:
-  print("model: Gaussian")
-  x = np.arange(N)
-  y = norm.cdf(x, loc=index_redlight, scale=N/4)
 else:
   raise ValueError()
 
