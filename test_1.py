d084e0da743ec27be67ad9d2260c2cafab11c1e1
Terry Boult <tboult@vast.uccs.edu>
2022-01-09 14:08:23 -0700

    TB adds a few more models

--- test_1.py
+++ test_1.py
@@ -36,6 +36,10 @@ model 10: Gaussian Bernoulli, has corrolatation with red light, no causation. ma
 
 --noise_threshold: float number between 0.0 and 1.0. default is 0.5. It is used in Cumulative Sum and Perturbation models. It does not affect other models.
 
+--noise_min:  floats  number between 0.0 and 1.0. default is 0.0.  It is used in Cumulative Sum and Perturbation models. It does not affect other models.
+
+--noise_max:  floats  number between 0.0 and 1.0. default is .3.0.  It is used in Cumulative Sum and Perturbation models. It does not affect other models.
+
 --output_csv_path: path for output csv file.
 
 '''
@@ -44,21 +48,29 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm
 import argparse
+import pdb
 
 parser = argparse.ArgumentParser(description="Tiger expriment")
 parser.add_argument('--model', type=int, help="model number, positive integer > 9", required=True)
 parser.add_argument('--length', type=int, help="test length, positive integer > 9", required=True)
 parser.add_argument('--redlight', type=int, help="redlight index, start from 1, integer", required=True)
 parser.add_argument('--noise_threshold', type=float, help="noise level between 0.0 and 1.0, default 0.5", required=True)
+parser.add_argument('--noise_min', type=float,default=0.0, help="Minimum backgorund noise level between 0.0 and 1.0, default 0.0")
+parser.add_argument('--noise_max', type=float, default=0.3, help="Max background noise level between 0.0 and 1.0, default 0.2", required=False)
+parser.add_argument('--signal', type=float, default=0.2, help="Signal level above  background noise (between 0 and 1), default 0.2", required=False)
 parser.add_argument('--output_csv_path', type=str, help="path to save csv output", required=True)
 
 args = parser.parse_args()
 
-assert args.model in list(range(1,11))
+assert args.model in list(range(1,15))
 assert args.length >= 10
 assert args.redlight >= 1
 assert args.noise_threshold >= 0.0
 assert args.noise_threshold <= 1.0
+assert args.noise_min >= 0.0
+assert args.noise_min <= 1.0
+assert args.noise_max >= 0.0
+assert args.noise_max <= 1.0
 
 index_redlight = args.redlight - 1
 N = args.length
@@ -112,7 +124,7 @@ elif args.model == 8:
 elif args.model == 9:
   print("model: Gaussian")
   x = np.arange(N)
-  y = norm.cdf(x, loc=index_redlight, scale=N/4)
+  y = norm.cdf(x, loc=indePx_redlight, scale=N/4)
   
 elif args.model == 10:
   print("model: Gaussian Bernoulli")
@@ -125,6 +137,42 @@ elif args.model == 10:
   z = np.cumsum(x)
   y = z / (2 * z[index_redlight])
   y[y>1.0] = 1.0
+
+elif args.model == 11:
+  print("model: Pure Cumulative Sum on Noisy signal")
+  x = np.random.uniform(low=args.noise_min, high=args.noise_max, size=N)
+  x[index_redlight:] += args.signal
+#  d = x / (N / 2)   don't want to scale.. better to have scaling  baked into the noise_max so its more understandable
+  d = x 
+  y = np.cumsum(d)
+  y[y>1.0] = 1.0
+
+elif args.model == 12:
+  print("model: Random guess model 12 of location  ")
+  y = np.zeros(N)
+  loc  =  int(N*.1 + np.random.normal(N/4, N/4))
+  loc = min(loc, N-1)
+  y[loc:] = 1.0
+
+elif args.model == 13:
+  print("model: Random guess model 13 of location  ")
+  y = np.zeros(N)
+  loc  =  int(N*.1 + np.random.normal(N/8, N/8))
+  loc = min(loc, N-1)
+  y[loc:] = 1.0
+
+
+elif args.model == 14:
+  print("model: Random guess model 13 of location  ")
+  y = np.zeros(N)
+  loc  =  int(N*.1 + np.random.uniform(N/8, N/3))
+  loc = min(loc, N-1)
+  y[loc:] = 1.0
+
+
+
+
+  
   
 else:
   raise ValueError()
